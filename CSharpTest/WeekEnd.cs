b901333990f5da3be71cfcbd37375e43064418fc
NyafProger <kuzmenko1704@gmail.com>
2020-08-07 17:02:33 +0300

    First Commit

---
+++ CSharpTest/WeekEnd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WeekEnd
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public WeekEnd(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            var endDate = startDate;
+            if (weekEnds == null)
+            {
+                endDate = endDate.AddDays(dayCount - 1);
+            }
+            else
+            {
+                endDate = WithWeekends(startDate, dayCount, weekEnds);
+            }
+            return endDate;
+        }
+
+        private DateTime WithWeekends(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            var endDate = startDate;
+            int i = 0;
+
+            foreach (WeekEnd weekEnd in weekEnds)
+            {
+                if (endDate < weekEnd.StartDate)
+                {
+                    break;
+                }
+                i++;
+            }
+
+            for (int j=0; j <= dayCount; j++)
+            {
+                if (endDate == weekEnds[i]?.StartDate)
+                {
+                    endDate = weekEnds[i].EndDate;
+                    i = i == weekEnds.Length ? i : i++;
+                }
+                else
+                {
+                    endDate = endDate.AddDays(1);
+                }
+            }
+            return endDate;
+        }
+    }
+}
